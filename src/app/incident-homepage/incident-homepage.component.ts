118b490df7acdbf60d3643756c61ab00f95ce125
pri <priyankaramesh101@gmail.com>
2023-10-17 16:52:42 +0530

    incident homepage done

--- src/app/incident-homepage/incident-homepage.component.ts
+++ src/app/incident-homepage/incident-homepage.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ServiceService, Incident } from '../service.service';
+import { MatTableDataSource } from '@angular/material/table';
+
 @Component({
   selector: 'app-incident-homepage',
   templateUrl: './incident-homepage.component.html',
@@ -7,8 +11,24 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class IncidentHomepageComponent {
 
-  constructor(private router: Router) { } 
-      
+  incidents: Incident[]=[];
+
+  constructor(private router: Router, private httpClient : HttpClient, private service : ServiceService) { } 
+  getLevel(n: undefined | number): string
+  {
+    if(n==0)
+    {
+      return "Low";
+    }
+    else if(n==1)
+    {
+      return "Medium";
+    }
+    else
+    {
+      return "High";
+    }
+  }  
   ngOnInit() { 
       this.router.events.subscribe((event) => { 
           if (!(event instanceof NavigationEnd)) { 
@@ -16,6 +36,22 @@ export class IncidentHomepageComponent {
           } 
           window.scrollTo(0, 0) 
       }); 
+
+      this.service.getIncidents().subscribe(Response=>{
+        this.incidents=Response;
+        console.log(this.incidents);
+        
+        
+      
+      },
+      error=>{
+        //need to display "invalid credentials, try again" in the bottom of the form. clear the password field
+        console.log(error);
+      });
+
   } 
 
+
+
 }
+
