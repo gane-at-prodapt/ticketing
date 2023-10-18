132a9a37ab68352686df0eeda7f4fabb82d682b4
Ganesan J <ganesan.j@prodapt.com>
2023-10-18 15:48:22 +0530

    access specified

--- src/app/network-homepage/network-homepage.component.ts
+++ src/app/network-homepage/network-homepage.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { ServiceService, NetworkElement } from '../service.service';
+import { ServiceService, NetworkElement,Access } from '../service.service';
+import { ToastrService } from 'ngx-toastr';
+import { getCookie } from 'typescript-cookie';
 
 @Component({
   selector: 'app-network-homepage',
@@ -11,8 +13,40 @@ import { ServiceService, NetworkElement } from '../service.service';
 export class NetworkHomepageComponent {
   
   networkElements: NetworkElement[]=[];
-  constructor(private router: Router, private httpClient : HttpClient, private service : ServiceService) { } 
- 
+  access:Access[] = [];
+
+  constructor(private router: Router, private httpClient : HttpClient, private service : ServiceService, private toastr: ToastrService) { } 
+
+  navCreateNetwork(){
+    if(this.access[0].status=="write"){
+      this.router.navigate(['/','addnetwork']);
+    }else{
+      this.toastr.error('Access restricted');
+    }
+    
+  }
+  navCreateIncident(){
+    if(this.access[2].status=="write"){
+      this.router.navigate(['/','ticket']);
+    }else{
+      this.toastr.error('Access restricted');
+    }
+  }
+  navCreateIssue(){
+    if(this.access[4].status=="write"){
+      this.router.navigate(['/','addissue']);
+    }else{
+      this.toastr.error('Access restricted');
+    }
+  }
+  navCreateUser(){
+    if(this.access[6].status=="write"){
+      this.router.navigate(['/','adduser']);
+    }else{
+      this.toastr.error('Access restricted');
+    }
+  }
+  
       
   ngOnInit() { 
       this.router.events.subscribe((event) => { 
@@ -25,12 +59,15 @@ export class NetworkHomepageComponent {
       this.service.getNetworkElements().subscribe(Response=>{
         this.networkElements=Response;
         console.log(this.networkElements);
-        
-        
-      
       },
       error=>{
         //need to display "invalid credentials, try again" in the bottom of the form. clear the password field
+        console.log(error);
+      });
+
+      this.service.getAccessByRole(Number(getCookie("userRoleId"))).subscribe(Response=>{
+        this.access=Response;
+      },error=>{
         console.log(error);
       });
   } 
--- src/app/roles-homepage/roles-homepage.component.ts
+++ src/app/roles-homepage/roles-homepage.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { ServiceService, Role } from '../service.service';
+import { ServiceService, Role,Access } from '../service.service';
+import { ToastrService } from 'ngx-toastr';
+import { getCookie } from 'typescript-cookie';
 
 @Component({
   selector: 'app-roles-homepage',
@@ -11,8 +13,40 @@ import { ServiceService, Role } from '../service.service';
 export class RolesHomepageComponent {
 
   roles: Role[]=[];
-  constructor(private router: Router, private httpClient : HttpClient, private service : ServiceService) { } 
-      
+  access:Access[] = [];
+
+  constructor(private router: Router, private httpClient : HttpClient, private service : ServiceService, private toastr: ToastrService) { } 
+
+  navCreateNetwork(){
+    if(this.access[0].status=="write"){
+      this.router.navigate(['/','addnetwork']);
+    }else{
+      this.toastr.error('Access restricted');
+    }
+    
+  }
+  navCreateIncident(){
+    if(this.access[2].status=="write"){
+      this.router.navigate(['/','ticket']);
+    }else{
+      this.toastr.error('Access restricted');
+    }
+  }
+  navCreateIssue(){
+    if(this.access[4].status=="write"){
+      this.router.navigate(['/','addissue']);
+    }else{
+      this.toastr.error('Access restricted');
+    }
+  }
+  navCreateUser(){
+    if(this.access[6].status=="write"){
+      this.router.navigate(['/','adduser']);
+    }else{
+      this.toastr.error('Access restricted');
+    }
+  }
+
   ngOnInit() { 
       this.router.events.subscribe((event) => { 
           if (!(event instanceof NavigationEnd)) { 
@@ -29,6 +63,12 @@ export class RolesHomepageComponent {
       },
       error=>{
         //need to display "invalid credentials, try again" in the bottom of the form. clear the password field
+        console.log(error);
+      });
+
+      this.service.getAccessByRole(Number(getCookie("userRoleId"))).subscribe(Response=>{
+        this.access=Response;
+      },error=>{
         console.log(error);
       });
   } 
